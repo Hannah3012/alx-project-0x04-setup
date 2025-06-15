cc15320224136de9757c8a3811347a8c9550c4c4
Hanna Tariku <hannatariku1996@gmail.com>
2025-06-15 07:38:41 +0300

    updated Header component

--- alx-project-0x06/components/layouts/Header.tsx
+++ alx-project-0x06/components/layouts/Header.tsx
@@ -8,7 +8,6 @@ const Header: React.FC = () => {
   const pathname = usePathname();
   const count = useSelector((state: RootState) => state.counter.value);
 
-
   return (
     <header className="fixed w-full bg-white shadow-md">
       <div className="container mx-auto flex justify-between items-center py-6 px-4 md:px-8">
@@ -29,15 +28,10 @@ const Header: React.FC = () => {
           ) : (
             <p className=" font-semibold text-lg">Current count : {count}</p>
           )}
-
         </div>
-
       </div>
     </header>
-
   );
 };
-
-
 
 export default Header;
